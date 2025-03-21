a978621b7b5641f0662158d5aa9bd38ac31e7b09
amogelangtsetse <105071248+amogelangtsetse@users.noreply.github.com>
2025-03-21 10:48:25 +0200

    patient appointment platform

---
+++ IntergatedProject.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package intergatedproject;
+
+/**
+ *
+ * @author amogelangtsetse
+ */
+public class IntergatedProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        frmMain2 frmMain2 = new frmMain2();
+        frmMain2.show();
+     
+        
+    }
+    
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Client-Appointment-Booking-System
